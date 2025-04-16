c4426d833c1cce1fdec230b0400247b58c61f21b
Kresa12 <aurakresa12@gmail.com>
2025-04-17 00:37:41 +0700

    [Database - Menambahkan Tabel Products]

--- database/migrations/2025_04_16_173040_create_products_table.php
+++ database/migrations/2025_04_16_173040_create_products_table.php
@@ -13,40 +13,19 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->unsignedBigInteger('id')->primary()->autoIncrement();
-
-            // Kolom name
             $table->string('name', 255)->nullable(false);
-
-            // Kolom slug
             $table->string('slug', 255)->unique();
-
-            // Kolom description
             $table->text('description')->nullable();
-
-            // Kolom sku
             $table->string('sku', 50)->unique();
-
-            // Kolom price
-            $table->decimal('price', 10, 2)->unsigned();
-
-            // Kolom stock
             $table->integer('stock')->default(0)->unsigned();
-
-            // Kolom product_category_id (foreign key)
             $table->unsignedBigInteger('product_category_id')->nullable();
             $table->foreign('product_category_id')
                   ->references('id')
                   ->on('product_categories')
                   ->onDelete('set null')
                   ->onUpdate('cascade');
-
-            // Kolom image_url
             $table->string('image_url', 255)->nullable();
-
-            // Kolom is_active
             $table->boolean('is_active')->default(true);
-
-            // Timestamps
             $table->timestamps();
         });
     }
